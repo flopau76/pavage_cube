e58b9d56734e57cb04c4179a86f21215afde8563
Florence Pautet <flopau76210@gmail.com>
2024-03-15 18:08:28 +0100

    debug results visualisation

--- draw.py
+++ draw.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.linalg import matrix_power
 import matplotlib.pyplot as plt
+import mayavi.mlab as mlab  # better than pyplot for 3d plot
+
 
 Rx = np.array([[1,0,0],[0,0,-1],[0,1,0]])
 Ry = np.array([[0,0,1],[0,1,0],[-1,0,0]])
@@ -8,6 +10,12 @@ Rz = np.array([[0,-1,0],[1,0,0],[0,0,1]])
 
 def coeff_to_matrix(coeff):
     return matrix_power(Rx, coeff[0]) @ matrix_power(Ry,coeff[1]) @ matrix_power(Rz,coeff[2])
+
+def get_position(piece, translation, rotation):
+    res = []
+    for c in piece:
+        res.append(rotation @ c + translation)
+    return np.array(res, dtype=int)
 
 def read_result(file):
     print(f"Reading file: {file}")
@@ -41,38 +49,38 @@ def read_result(file):
         rotation = [coeff_to_matrix(r) for r in rotation]
         return translation, rotation
 
-def get_position(piece, translation, rotation):
-    res = []
-    for c in piece:
-        res.append(rotation @ c + translation)
-    return np.array(res, dtype=int)
-
-def draw_piece(piece, color='b', axes=None):
-    if axes is not None:
-        ax = axes
-    else:
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-    ax.plot3D(piece[:,0], piece[:,1], piece[:,2], color)
-
 def draw_cube(piece, T,R):
+    """3d plot using matplotlib: has some display artifacts were a back object is displayed in front of a front object"""
     n = len(R)
     colors = plt.cm.jet(np.linspace(0,1,n))
     print(colors)
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     for i in range(n):
-        piece = get_position(piece, T[i],R[i])
-        ax.plot3D(piece[:,0], piece[:,1], piece[:,2], color=colors[i])
+        position = get_position(piece, T[i],R[i])
+        print(position)
+        ax.plot3D(position[:,0], position[:,1], position[:,2], color=colors[i])
     plt.show()
+
+def draw_cube2(piece, T,R):
+    n = len(R)
+    colors = [plt.cm.get_cmap('Spectral')(i/(n-1))[:3] for i in range(n) ]
+    print(colors)
+    for i in range(len(T)):
+        position = get_position(piece, T[i],R[i])/4 # necessity to be between 0 and 1 to work with mlab
+        print(position)
+        mlab.plot3d(position[:,0], position[:,1], position[:,2], color=colors[i])
+        # mlab.points3d(position[:,0], position[:,1], position[:,2], color=colors[i])
+    mlab.show()
         
 
 if __name__ == '__main__':
 
-    piece3 = [[0,0,1],[0,0,0],[0,1,0]]
+    piece3 = [[0,1,0],[0,0,0],[0,0,1]]
     piece4 = [[0,1,0],[0,0,0],[0,0,2]]
 
-    T, R = read_result('cube3x3.res')
-    piece = np.array(piece3)
-    
-    draw_cube(piece, T,R)
+    T3, R3 = read_result('cube3x3.res')
+    T4, R4 = read_result('cube4x4.res')
+
+    draw_cube2(piece3, T3, R3)
+    draw_cube2(piece4, T4, R4)
